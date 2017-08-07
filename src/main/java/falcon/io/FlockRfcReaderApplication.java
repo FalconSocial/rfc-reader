07a9edbe3c12a32b85d14e197f9cb920baf24aee
Tibor Nagy <tibor@falcon.io>
2017-08-07 13:19:56 +0200

    project skeleton

---
+++ src/main/java/falcon/io/FlockRfcReaderApplication.java
@@ -0,0 +1,12 @@
+package falcon.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlockRfcReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlockRfcReaderApplication.class, args);
+	}
+}
---
+++ src/main/java/falcon/io/resource/RFCReaderResource.java
@@ -0,0 +1,11 @@
+package falcon.io.resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by tibor on 2017-08-07.
+ */
+@Controller
+public class RFCReaderResource {
+}
