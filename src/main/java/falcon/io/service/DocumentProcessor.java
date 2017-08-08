239ff25983fe4d3170dd0dfee71d1089b3ce63eb
Tibor Nagy <tibor@falcon.io>
2017-08-08 13:52:01 +0200

    add UUID to document processor

--- src/main/java/falcon/io/service/DocumentProcessor.java
+++ src/main/java/falcon/io/service/DocumentProcessor.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 /**
  * Created by tibor on 2017-08-07.
@@ -20,20 +21,20 @@ public class DocumentProcessor {
 
     }
 
-    public void processDocument(String document){
+    public void processDocument(String document, UUID documentId){
         ArrayList<ArrayList<Word>> pages = StringSplitter.split(document);
         for(int i=0; i< pages.size(); i++){
-            processPage(pages.get(i), i);
+            processPage(pages.get(i), i, documentId);
         }
     }
 
-    private void processPage(ArrayList<Word> page, int pageIndex){
+    private void processPage(ArrayList<Word> page, int pageIndex, UUID documentId){
         for(int i=0; i< page.size(); i++){
-            processWord(page.get(i),pageIndex, i);
+            processWord(page.get(i),pageIndex, i, documentId);
         }
     }
 
-    private void processWord(Word word, int pageIndex, int wordIndex){
+    private void processWord(Word word, int pageIndex, int wordIndex, UUID documentId){
 
     }
 
